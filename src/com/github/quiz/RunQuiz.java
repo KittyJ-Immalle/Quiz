80f0ce3cb4d39b98104f78e6915eaf1ff562af85
Kitty Jacobs <kitty.jacobs@immalle.eu>
2017-11-09 21:02:58 +0100

    Added functions + questions

--- src/com/github/quiz/RunQuiz.java
+++ src/com/github/quiz/RunQuiz.java
@@ -1,18 +1,55 @@
 package com.github.quiz;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RunQuiz extends ReadQuiz {
 
+
 	public static void main(String[] args) {
 		
-		ReadQuiz readquiz = new ReadQuiz();
+		Scanner scanInput = new Scanner(System.in);
+		String YesNo;
+		Run(scanInput);
+		
+		while(true) {
+			System.out.println("Do you want to play again?");
+			YesNo = scanInput.nextLine().toLowerCase();
+			if(YesNo.equals("yes")) {
+				Run(scanInput);
+			} else if(YesNo.equals("no")) {
+				System.out.println("You may now go, yes, go.");
+				break;
+			} else {
+				System.out.println("Please only enter yes or no.");
+			}
+		}
+		scanInput.close();
+		
+		
+	}
+	
+	public static void Run(Scanner scanInput) {
+		ReadQuiz readQuiz = new ReadQuiz();
 		ArrayList<String> questions = new ArrayList<String>();
-		
-		questions = (ArrayList<String>) readquiz.Read("questions.txt");
-		System.out.println(questions);
-		
-
+		questions = (ArrayList<String>) readQuiz.Read("questions.txt");
+		for(int i = 0; i < questions.size(); i = i + 2) {
+			
+			System.out.println(questions.get(i));
+			
+			String input = scanInput.nextLine().toLowerCase();
+			String[] answer = questions.get(i + 1).toLowerCase().split(",");
+			
+			for(int j = 0; j < answer.length; j++) {
+				if (input.equals(answer[j])) {
+					System.out.println("Correct");
+					break;
+				} else if (j == answer.length - 1){
+					System.out.println("Wrong");
+					System.out.println(answer[j]);
+				}
+			}
+		}
 	}
 
 }
